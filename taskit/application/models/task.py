52e498ffe81a51c9373f3a8c05bf85973047a0ba
Esteban Echeverry <eecheverry@nubark.com>
2018-01-10 22:30:14 -0500

    Agregar atributos de las tareas, con sus valores por defecto

--- taskit/application/models/task.py
+++ taskit/application/models/task.py
@@ -1,3 +1,12 @@
+from datetime import datetime
+
+
 class Task:
     def __init__(self, name: str) -> None:
         self.name = name
+        self.uid = ""
+        self.due_date = datetime.now()
+        self.priority = 1
+        self.project_id = ""
+        self.stage_id = ""
+        self.comments = ""
