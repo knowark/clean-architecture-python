388e0cc31347c029cb6910d468a0fc4dcdba61cf
Esteban Echeverry <eecheverry@nubark.com>
2018-01-10 22:30:14 -0500

    Agregar atributos de las tareas, con sus valores por defecto

--- tests/application/models/test_task.py
+++ tests/application/models/test_task.py
@@ -1,10 +1,22 @@
-import taskit
+import datetime
+import pytest
 from taskit.application.models.task import Task
 
 
-def test_task_creation() -> None:
+@pytest.fixture
+def task() -> pytest.fixture:
     name = "Buy the milk"
+    return Task(name=name)
 
-    task = Task(name=name)
 
-    assert task.name == name
+def test_task_creation(task: pytest.fixture) -> None:
+    assert task.name == "Buy the milk"
+
+
+def test_task_default_attributes(task: pytest.fixture) -> None:
+    assert task.uid == ""
+    assert isinstance(task.due_date, datetime.datetime)
+    assert task.priority == 1
+    assert task.project_id == ""
+    assert task.stage_id == ""
+    assert task.comments == ""
