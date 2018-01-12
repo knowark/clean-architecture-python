84203c19660ea9ba339179647308639679c2f665
Esteban Echeverry <eecheverry@nubark.com>
2018-01-12 08:10:26 -0500

    Refactorizar imports en los tests y arreglar typings

--- tests/application/models/test_task.py
+++ tests/application/models/test_task.py
@@ -1,21 +1,21 @@
-import datetime
-import pytest
+from datetime import datetime
+from pytest import fixture
 from taskit.application.models.task import Task
 
 
-@pytest.fixture
-def task() -> pytest.fixture:
+@fixture
+def task() -> Task:
     name = "Buy the milk"
     return Task(name=name)
 
 
-def test_task_creation(task: pytest.fixture) -> None:
+def test_task_creation(task: Task) -> None:
     assert task.name == "Buy the milk"
 
 
-def test_task_default_attributes(task: pytest.fixture) -> None:
+def test_task_default_attributes(task: Task) -> None:
     assert task.uid == ""
-    assert isinstance(task.due_date, datetime.datetime)
+    assert isinstance(task.due_date, datetime)
     assert task.priority == 1
     assert task.project_id == ""
     assert task.stage_id == ""
