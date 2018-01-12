00a9ba0a137a66cb897918f4779ed54d16136fd2
Esteban Echeverry <eecheverry@nubark.com>
2018-01-12 08:10:26 -0500

    Refactorizar imports en los tests y arreglar typings

--- tests/application/models/test_project.py
+++ tests/application/models/test_project.py
@@ -1,18 +1,18 @@
-import pytest
+from pytest import fixture
 from taskit.application.models.project import Project
 
 
-@pytest.fixture
-def project() -> pytest.fixture:
+@fixture
+def project() -> Project:
     name = "Personal"
     return Project(name=name)
 
 
-def test_project_creation(project: pytest.fixture) -> None:
+def test_project_creation(project: Project) -> None:
     assert project.name == "Personal"
 
 
-def test_project_default_attributes(project: pytest.fixture) -> None:
+def test_project_default_attributes(project: Project) -> None:
     assert project.uid == ""
     assert project.stage_ids == []
     assert project.comments == ""
--- tests/application/models/test_stage.py
+++ tests/application/models/test_stage.py
@@ -1,18 +1,17 @@
-import datetime
-import pytest
+from pytest import fixture
 from taskit.application.models.stage import Stage
 
 
-@pytest.fixture
-def stage() -> pytest.fixture:
+@fixture
+def stage() -> Stage:
     name = "backlog"
     return Stage(name=name)
 
 
-def test_stage_creation(stage: pytest.fixture) -> None:
+def test_stage_creation(stage: Stage) -> None:
     assert stage.name == "backlog"
 
 
-def test_stage_default_attributes(stage: pytest.fixture) -> None:
+def test_stage_default_attributes(stage: Stage) -> None:
     assert stage.uid == ""
     assert stage.closure is False
