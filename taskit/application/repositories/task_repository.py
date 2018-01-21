d278bdd59109552b210c3c24bc6364701bafdc3b
Esteban Echeverry <eecheverry@nubark.com>
2018-01-21 12:04:00 -0500

    Crear implementación en memoria de TaskRepository

--- taskit/application/repositories/task_repository.py
+++ taskit/application/repositories/task_repository.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Dict
 from taskit.application.models.task import Task
 
 
@@ -10,3 +11,17 @@ class TaskRepository(ABC):
     @abstractmethod
     def get(self, uid: str) -> Task:
         "Get method to be implemented."
+
+
+class MemoryTaskRepository(TaskRepository):
+    def __init__(self) -> None:
+        self.tasks = {}  # type: Dict[str, Task]
+
+    def add(self, task: Task) -> bool:
+        "Add method to be implemented."
+
+    def get(self, uid: str) -> Task:
+        "Get method to be implemented."
+
+    def load(self, tasks_dict: Dict[str, Task]) -> None:
+        self.tasks = tasks_dict
--- tests/application/repositories/test_task_repository.py
+++ tests/application/repositories/test_task_repository.py
@@ -1,7 +1,24 @@
-from taskit.application.repositories.task_repository import TaskRepository
+from pytest import fixture
+from taskit.application.models.task import Task
+from taskit.application.repositories.task_repository import (
+    TaskRepository,
+    MemoryTaskRepository)
 
 
 def test_task_repository_methods() -> None:
     abstract_methods = TaskRepository.__abstractmethods__
     assert 'add' in abstract_methods
     assert 'get' in abstract_methods
+
+
+def test_task_repository_memory_implementation() -> None:
+    assert issubclass(MemoryTaskRepository, TaskRepository)
+
+
+def test_memory_task_repository_load() -> None:
+    memory_task_repository = MemoryTaskRepository()
+    tasks_dict = {
+        'T-1': Task("Buy the milk")
+    }
+    memory_task_repository.load(tasks_dict)
+    assert memory_task_repository.tasks == tasks_dict
