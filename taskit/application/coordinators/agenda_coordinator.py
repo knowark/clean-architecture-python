879c7642adf8fe7a418158021fa5098f561e1445
Esteban Echeverry <eecheverry@nubark.com>
2018-01-29 23:18:30 -0500

    Crear AgendaCoordinator para gestionar las operaciones entre tareas, proyectos y etapas

---
+++ taskit/application/coordinators/agenda_coordinator.py
@@ -0,0 +1,5 @@
+
+
+class AgendaCoordinator:
+    def __init__(self):
+        pass
---
+++ tests/application/coordinators/test_agenda_coordinator.py
@@ -0,0 +1,6 @@
+from taskit.application.coordinators.agenda_coordinator import (
+    AgendaCoordinator)
+
+
+def test_agenda_coordinator_creation():
+    agenda_coordinator = AgendaCoordinator()
