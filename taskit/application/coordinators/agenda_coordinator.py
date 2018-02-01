306de2f35fc992fb67cf00f93d8a834ae773dce7
Esteban Echeverry <eecheverry@nubark.com>
2018-01-31 22:03:06 -0500

    Definir método para crear nuevas tareas

--- taskit/application/coordinators/agenda_coordinator.py
+++ taskit/application/coordinators/agenda_coordinator.py
@@ -3,3 +3,6 @@
 class AgendaCoordinator:
     def __init__(self):
         pass
+
+    def create_new_task(self):
+        pass
--- tests/application/coordinators/test_agenda_coordinator.py
+++ tests/application/coordinators/test_agenda_coordinator.py
@@ -4,3 +4,4 @@ from taskit.application.coordinators.agenda_coordinator import (
 
 def test_agenda_coordinator_creation():
     agenda_coordinator = AgendaCoordinator()
+    assert hasattr(agenda_coordinator, 'create_new_task')
