a901fb8bcae6095c423cbfd3c9bb06d8fa109ae4
Esteban Echeverry <eecheverry@nubark.com>
2018-02-01 20:25:09 -0500

    Agregar la capacidad de crear etapas a partir de un diccionario

--- taskit/application/models/stage.py
+++ taskit/application/models/stage.py
@@ -1,5 +1,6 @@
 class Stage:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, **kwargs) -> None:
         self.name = name
         self.uid = ""
         self.closure = False
+        self.__dict__.update(kwargs)
--- tests/application/models/test_stage.py
+++ tests/application/models/test_stage.py
@@ -15,3 +15,15 @@ def test_stage_creation(stage: Stage) -> None:
 def test_stage_default_attributes(stage: Stage) -> None:
     assert stage.uid == ""
     assert stage.closure is False
+
+
+def test_stage_initilization_from_dict():
+    stage_dict = {
+        'name': "Done",
+        'uid': "S-001",
+        'closure': True
+    }
+    stage = Stage(**stage_dict)
+    assert stage.name == "Done"
+    assert stage.uid == "S-001"
+    assert stage.closure is True
