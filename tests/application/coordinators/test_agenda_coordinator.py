62a62dea5ae5c98cfa21a7881f94cea3fc89f959
Esteban Echeverry <eecheverry@nubark.com>
2018-02-01 22:49:50 -0500

    Crear estructura preliminar de los métodos del agenda_coordinator

--- tests/application/coordinators/test_agenda_coordinator.py
+++ tests/application/coordinators/test_agenda_coordinator.py
@@ -1,7 +1,30 @@
+
+from datetime import datetime
+from pytest import fixture
 from taskit.application.coordinators.agenda_coordinator import (
     AgendaCoordinator)
 
 
-def test_agenda_coordinator_creation():
-    agenda_coordinator = AgendaCoordinator()
+@fixture
+def agenda_coordinator() -> AgendaCoordinator:
+    return AgendaCoordinator()
+
+
+def test_agenda_coordinator_creation(
+        agenda_coordinator: AgendaCoordinator) -> None:
     assert hasattr(agenda_coordinator, 'create_new_task')
+
+
+# def test_aqenda_coordinator_create_new_task(
+#         self, agenda_coordinator: AgendaCoordinator) -> None:
+#     task_dict = {
+#         'name': "Buy bread and eggs",
+#         'due_date':  datetime.strptime("2018-02-15", '%Y-%m-%d'),
+#         'priority': 3,
+#         'project_id': "P-1",
+#         'stage_id': "S-1",
+#         'comments': "The supermarket closes at 8 pm."
+#     }
+
+#     agenda_coordinator.create_new_task(task_dict)
+    
