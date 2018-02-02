5165e247e460297d178ff14906425706057a6d10
Esteban Echeverry <eecheverry@nubark.com>
2018-02-01 20:44:07 -0500

    Permitir crear un proyecto a través de diccionarios

--- taskit/application/models/project.py
+++ taskit/application/models/project.py
@@ -2,8 +2,9 @@ from typing import List
 
 
 class Project:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, **kwargs) -> None:
         self.name = name
         self.uid = ""
         self.stage_ids = []  # type: List[str]
         self.comments = ""
+        self.__dict__.update(kwargs)
--- tests/application/models/test_project.py
+++ tests/application/models/test_project.py
@@ -16,3 +16,18 @@ def test_project_default_attributes(project: Project) -> None:
     assert project.uid == ""
     assert project.stage_ids == []
     assert project.comments == ""
+
+
+def test_project_initilization_from_dict():
+    project_dict = {
+        'name': "Done",
+        'uid': "S-001",
+        'stage_ids': ['Draft', 'Progress', 'Done'],
+        'comments': "This is the final stage."
+    }
+
+    project = Project(**project_dict)
+    assert project.name == "Done"
+    assert project.uid == "S-001"
+    assert project.stage_ids == ['Draft', 'Progress', 'Done']
+    assert project.comments == "This is the final stage."
