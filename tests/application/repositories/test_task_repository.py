2c29b85306d2255936cc324532fdf9b66ba03c8a
Esteban Echeverry <eecheverry@nubark.com>
2018-02-01 23:29:11 -0500

    Agregar type:  ignore al llamado de los metodos abstractos en las pruebas para evitar falsos positivos en mypy.

--- tests/application/repositories/test_task_repository.py
+++ tests/application/repositories/test_task_repository.py
@@ -7,7 +7,7 @@ from taskit.application.repositories.task_repository import (
 
 
 def test_task_repository_methods() -> None:
-    abstract_methods = TaskRepository.__abstractmethods__
+    abstract_methods = TaskRepository.__abstractmethods__  # type: ignore
     assert 'add' in abstract_methods
     assert 'get' in abstract_methods
     assert 'update' in abstract_methods
