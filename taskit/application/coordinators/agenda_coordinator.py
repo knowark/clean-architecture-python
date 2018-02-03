d55d1b8fbf5070f93b6406d416dd3d024238ffe6
Esteban Echeverry <eecheverry@nubark.com>
2018-02-03 09:03:38 -0500

    Implementar método de create_task en el AgendaCoordinator

--- taskit/application/coordinators/agenda_coordinator.py
+++ taskit/application/coordinators/agenda_coordinator.py
@@ -1,8 +1,29 @@
+from typing import Dict
+from taskit.application.models.task import Task
+from taskit.application.repositories.project_repository import ProjectRepository
+from taskit.application.repositories.task_repository import TaskRepository
 
 
 class AgendaCoordinator:
-    def __init__(self):
-        pass
+    def __init__(self, project_repository: ProjectRepository,
+                 task_repository: TaskRepository) -> None:
+        self.task_repository = task_repository
+        self.project_repository = project_repository
 
-    def create_new_task(self):
-        pass
+    def create_task(self, task_dict: Dict[str, any]):
+        project_id = task_dict.get('project_id')
+        # Validate that the project exists
+        self.project_repository.get(project_id)
+
+        # Intantiate a Task object
+        task = Task(**task_dict)
+
+        # Set the task stage to 'new'
+        task.stage = 'new' 
+
+        # Add the new task to the task repository
+        self.task_repository.add(task)
+
+
+
+        
--- tests/application/coordinators/test_agenda_coordinator.py
+++ tests/application/coordinators/test_agenda_coordinator.py
@@ -1,30 +1,69 @@
 
 from datetime import datetime
 from pytest import fixture
+from taskit.application.models.project import Project
+from taskit.application.models.task import Task
+from taskit.application.repositories.project_repository import (
+    ProjectRepository, MemoryProjectRepository)
+from taskit.application.repositories.task_repository import (
+    TaskRepository, MemoryTaskRepository)
 from taskit.application.coordinators.agenda_coordinator import (
     AgendaCoordinator)
 
 
 @fixture
-def agenda_coordinator() -> AgendaCoordinator:
-    return AgendaCoordinator()
+def project_repository() -> MemoryProjectRepository:
+    project_repository = MemoryProjectRepository()
+    projects_dict = {
+        'P-1': Project("Personal"),
+        'P-2': Project("Work"),
+        'P-3': Project("Errands")
+    }
+    project_repository.sequence = 4
+    project_repository.load(projects_dict)
+    return project_repository
+
+
+@fixture
+def task_repository() -> MemoryTaskRepository:
+    task_repository = MemoryTaskRepository()
+    tasks_dict = {
+        'T-1': Task("Buy the milk"),
+        'T-2': Task("Make conference presentation"),
+        'T-3': Task("Clean the kitchen")
+    }
+    task_repository.sequence = 4
+    task_repository.load(tasks_dict)
+    return task_repository
+
+
+@fixture
+def agenda_coordinator(
+        project_repository: ProjectRepository,
+        task_repository: TaskRepository) -> AgendaCoordinator:
+    return AgendaCoordinator(project_repository, task_repository)
 
 
 def test_agenda_coordinator_creation(
         agenda_coordinator: AgendaCoordinator) -> None:
-    assert hasattr(agenda_coordinator, 'create_new_task')
+    assert hasattr(agenda_coordinator, 'create_task')
 
 
-# def test_aqenda_coordinator_create_new_task(
-#         self, agenda_coordinator: AgendaCoordinator) -> None:
-#     task_dict = {
-#         'name': "Buy bread and eggs",
-#         'due_date':  datetime.strptime("2018-02-15", '%Y-%m-%d'),
-#         'priority': 3,
-#         'project_id': "P-1",
-#         'stage_id': "S-1",
-#         'comments': "The supermarket closes at 8 pm."
-#     }
+def test_aqenda_coordinator_create_task(
+        agenda_coordinator: AgendaCoordinator) -> None:
+    task_dict = {
+        'name': "Buy bread and eggs",
+        'due_date':  datetime.strptime("2018-02-15", '%Y-%m-%d'),
+        'priority': 3,
+        'project_id': "P-1",
+        'comments': "The supermarket closes at 8 pm."
+    }
 
-#     agenda_coordinator.create_new_task(task_dict)
-    
+    agenda_coordinator.create_task(task_dict)
+
+    task_repository = agenda_coordinator.task_repository
+    task = task_repository.get('T-4')
+    assert task.name == task_dict['name']
+    assert task.project_id == task_dict['project_id']
+    assert task.stage == 'new'
+
